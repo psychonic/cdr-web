22a44a0eb6d7115671a212b48410427fb06f731b
azuisleet <azuisleet@localhost>
2011-05-06 22:53:14 -0600

    Baseline CDR reading into type

---
+++ CDRUpdater/CDRUpdater/DebugLog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CDRUpdater
+{
+    static class DebugLog
+    {
+        public const string Filename = "debug.log";
+
+        static string SessionFile;
+
+        static DebugLog()
+        {
+            SessionFile = Path.Combine(Environment.CurrentDirectory, Filename);
+        }
+
+        public static void Write(string format, params object[] args)
+        {
+            try
+            {
+                string line = string.Format("[ {0} ] {1}", DateTime.Now.ToLongTimeString(), string.Format(format, args));
+                File.AppendAllText(SessionFile, line);
+                Console.Write(line);
+            }
+            catch { }
+        }
+    }
+}
