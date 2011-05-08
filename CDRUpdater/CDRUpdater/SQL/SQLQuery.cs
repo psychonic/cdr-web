e05b4fcbb165706730fe5a0c1496b198641b0cf6
azuisleet <azuisleet@localhost>
2011-05-08 05:25:10 -0600

    app/sub capture

--- CDRUpdater/CDRUpdater/SQL/SQLQuery.cs
+++ CDRUpdater/CDRUpdater/SQL/SQLQuery.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using SteamKit2;
 using Jayrock.Json.Conversion;
+using System.Data;
 
 namespace CDRUpdater
 {
@@ -11,7 +12,7 @@ namespace CDRUpdater
     {
         private static CacheContext SQLContext = new CacheContext();
 
-        public static string BuildInsertQueryFromType(string table, object x)
+        /*public static string BuildDataInsertFromType(object x)
         {
             Type otype = x.GetType();
             Dictionary<string, string> sqlDict = new Dictionary<string, string>();
@@ -26,12 +27,64 @@ namespace CDRUpdater
                 sqlDict.Add(cattrib.Column, GetStringValue(field.PropertyType, field.GetValue(x, null)));
             }
 
-            return String.Format("INSERT INTO {2} ({0}) VALUES ({1});", String.Join(", ", sqlDict.Keys), String.Join(", ", sqlDict.Values), table);
+            return String.Join("\t", sqlDict.Values);
+        }*/
+
+        public static void BuildDataInsertFromTypeWithChanges(object x, DataRow prev_data, int cdr_id, int prev_cdr_id, out string sql_data, out string sql_data_capture)
+        {
+            Type otype = x.GetType();
+            Dictionary<string, string> sqlDict = new Dictionary<string, string>();
+            Dictionary<string, string> sqlCaptureDict = new Dictionary<string, string>();
+            int capture_count = -1;
+
+            foreach (var field in otype.GetCachedPropertyInfo(SQLContext))
+            {
+                SqlColumnAttribute cattrib = field.GetAttribute<SqlColumnAttribute>(SQLContext);
+
+                if (cattrib == null)
+                    continue;
+
+                bool enclose = false;
+                string type_value = GetStringValue(field.PropertyType, field.GetValue(x, null), true, out enclose);
+
+                if (prev_data != null)
+                {
+                    bool inner_enclose;
+
+                    string value_current = GetStringValue(field.PropertyType, prev_data[cattrib.Column], false, out inner_enclose);
+
+                    if (value_current != type_value || capture_count < 0)
+                    {
+                        sqlCaptureDict.Add(cattrib.Column, EscapeValue(value_current, inner_enclose));
+                        capture_count++;
+                    }
+                    else
+                    {
+                        sqlCaptureDict.Add(cattrib.Column, @"\N");
+                    }
+                }
+
+                type_value = EscapeValue(type_value, enclose);
+
+                sqlDict.Add(cattrib.Column, type_value);
+
+            }
+
+            sql_data = String.Join("\t", sqlDict.Values);
+
+            if (prev_data == null || capture_count <= 0)
+            {
+                sql_data_capture = null;
+                return;
+            }
+
+            sql_data_capture = String.Format("{0}\t{1}", prev_cdr_id, String.Join("\t", sqlCaptureDict.Values));
         }
 
-        private static string GetStringValue(Type propType, object value)
+        private static string GetStringValue(Type propType, object value, bool follow_types, out bool enclose)
         {
             string data = null;
+            enclose = false;
 
             if (propType.IsEnum)
             {
@@ -40,24 +93,36 @@ namespace CDRUpdater
             Type generic;
             int genericCount;
 
-            if (propType.IsTypeListOrDictionary(out generic, out genericCount))
+            if (follow_types && propType.IsTypeListOrDictionary(out generic, out genericCount))
             {
                 if (value == null)
                     value = new object[] { };
 
                 data = JsonConvert.ExportToString(value);
+                enclose = true;
             }
             else
             {
-                data = Convert.ToString(value);
+                if (propType == typeof(string))
+                    enclose = true;
+
+                if (propType == typeof(bool))
+                    data = ((bool)value) ? "1" : "0";
+                else
+                    data = Convert.ToString(value);
             }
 
-            return EscapeValue(data);
+            return data;
         }
 
-        private static string EscapeValue(string input)
+        private static string EscapeValue(string input, bool enclose)
         {
-            return '"' + input.Replace("\0", "\\0").Replace("\\", "\\\\").Replace("\'", "\\\'").Replace("\"", "\\\"") + '"';
+            string clean = input.Replace(@"\", @"\\").Replace("\0", "\\0").Replace("\'", "\\\'").Replace("\"", "\\\"");
+
+            if (enclose)
+                return clean; //'"' + clean + '"';
+            else
+                return clean;
         }
     }
 }
--- cdr_web-db.sql
+++ cdr_web-db.sql
@@ -4,7 +4,7 @@
 --
 -- Host: 127.0.0.1:3306
 
--- Generation Time: May 08, 2011 at 07:19 AM
+-- Generation Time: May 08, 2011 at 12:24 PM
 -- Server version: 5.5.11
 -- PHP Version: 5.3.6
 
@@ -43,32 +43,27 @@ CREATE TABLE `app` (
   PRIMARY KEY (`app_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE `app_state_capture` (
-  `cdr_id` int(11) NOT NULL,
+--
+-- Dumping data for table `app`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `apps_subs`
+--
+
+CREATE TABLE `apps_subs` (
   `app_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `install_dir` varchar(255) NOT NULL,
-  `min_cache_size` smallint(5) NOT NULL,
-  `max_cache_size` smallint(5) NOT NULL,
-  `launch_options` text NOT NULL,
-  `on_first_launch` int(11) NOT NULL,
-  `is_bandwidth_greedy` tinyint(1) NOT NULL,
-  `current_version_id` int(11) NOT NULL,
-  `filesystems` text NOT NULL,
-  `trickle_version_id` int(11) NOT NULL,
-  `user_defined` text NOT NULL,
-  `beta_version_password` varchar(255) NOT NULL,
-  `beta_version_id` int(11) NOT NULL,
-  `legacy_install_dir` varchar(255) NOT NULL,
-  `skip_mfp_overwrite` tinyint(1) NOT NULL,
-  `use_filesystem_dvr` tinyint(1) NOT NULL,
-  `manifest_only` tinyint(1) NOT NULL,
-  `app_of_manifest_only` int(11) NOT NULL,
-  PRIMARY KEY (`cdr_id`,`app_id`)
+  `sub_id` int(11) NOT NULL,
+  `cdr_id` int(11) NOT NULL,
+  KEY `app_id` (`app_id`,`sub_id`),
+  KEY `cdr_id` (`cdr_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `app`
+-- Dumping data for table `apps_subs`
 --
 
 
@@ -91,6 +86,41 @@ CREATE TABLE `app_filesystem` (
 
 --
 -- Dumping data for table `app_filesystem`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `app_state_capture`
+--
+
+CREATE TABLE `app_state_capture` (
+  `cdr_id` int(11) NOT NULL,
+  `app_id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `install_dir` varchar(255) DEFAULT NULL,
+  `min_cache_size` smallint(5) DEFAULT NULL,
+  `max_cache_size` smallint(5) DEFAULT NULL,
+  `launch_options` text,
+  `on_first_launch` int(11) DEFAULT NULL,
+  `is_bandwidth_greedy` tinyint(1) DEFAULT NULL,
+  `current_version_id` int(11) DEFAULT NULL,
+  `filesystems` text,
+  `trickle_version_id` int(11) DEFAULT NULL,
+  `user_defined` text,
+  `beta_version_password` varchar(255) DEFAULT NULL,
+  `beta_version_id` int(11) DEFAULT NULL,
+  `legacy_install_dir` varchar(255) DEFAULT NULL,
+  `skip_mfp_overwrite` tinyint(1) DEFAULT NULL,
+  `use_filesystem_dvr` tinyint(1) DEFAULT NULL,
+  `manifest_only` tinyint(1) DEFAULT NULL,
+  `app_of_manifest_only` int(11) DEFAULT NULL,
+  PRIMARY KEY (`cdr_id`,`app_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `app_state_capture`
 --
 
 
@@ -129,7 +159,7 @@ CREATE TABLE `app_version` (
 
 CREATE TABLE `cdr` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `hash` varchar(20) NOT NULL,
+  `hash` varchar(40) NOT NULL,
   `version` int(11) NOT NULL,
   `date_updated` datetime NOT NULL,
   `date_processed` datetime NOT NULL,
@@ -173,40 +203,45 @@ CREATE TABLE `sub` (
   PRIMARY KEY (`sub_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE `sub_state_capture` (
-  `cdr_id` int(11) NOT NULL,
-  `sub_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `billing_type` varchar(100) NOT NULL,
-  `cost_in_cents` int(11) NOT NULL,
-  `period_in_minutes` int(11) NOT NULL,
-  `on_subscribe_run_app_id` int(11) NOT NULL,
-  `on_subscribe_run_launch_option_index` int(11) NOT NULL,
-  `rate_limits` text NOT NULL,
-  `discounts` text NOT NULL,
-  `is_preorder` tinyint(1) NOT NULL,
-  `requires_shipping_address` tinyint(1) NOT NULL,
-  `domestic_cost_in_cents` int(11) NOT NULL,
-  `international_cost_in_cents` int(11) NOT NULL,
-  `required_key_type` int(11) NOT NULL,
-  `is_cyber_cafe` tinyint(1) NOT NULL,
-  `game_code` int(11) NOT NULL,
-  `game_code_description` varchar(255) NOT NULL,
-  `is_disabled` tinyint(1) NOT NULL,
-  `requires_cd` tinyint(1) NOT NULL,
-  `territory_code` int(11) NOT NULL,
-  `is_steam3_subscription` tinyint(1) NOT NULL,
-  `extended_info` text NOT NULL,
-  PRIMARY KEY (`cdr_id`,`sub_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
 --
 -- Dumping data for table `sub`
 --
 
-CREATE TABLE `apps_subs` (
-  `app_id` int(11) NOT NULL,
-  `sub_id` int(11) NOT NULL,
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sub_state_capture`
+--
+
+CREATE TABLE `sub_state_capture` (
   `cdr_id` int(11) NOT NULL,
-  KEY `app_id` (`app_id`,`sub_id`)
+  `sub_id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `billing_type` varchar(100) DEFAULT NULL,
+  `cost_in_cents` int(11) DEFAULT NULL,
+  `period_in_minutes` int(11) DEFAULT NULL,
+  `on_subscribe_run_app_id` int(11) DEFAULT NULL,
+  `on_subscribe_run_launch_option_index` int(11) DEFAULT NULL,
+  `rate_limits` text,
+  `discounts` text,
+  `is_preorder` tinyint(1) DEFAULT NULL,
+  `requires_shipping_address` tinyint(1) DEFAULT NULL,
+  `domestic_cost_in_cents` int(11) DEFAULT NULL,
+  `international_cost_in_cents` int(11) DEFAULT NULL,
+  `required_key_type` int(11) DEFAULT NULL,
+  `is_cyber_cafe` tinyint(1) DEFAULT NULL,
+  `game_code` int(11) DEFAULT NULL,
+  `game_code_description` varchar(255) DEFAULT NULL,
+  `is_disabled` tinyint(1) DEFAULT NULL,
+  `requires_cd` tinyint(1) DEFAULT NULL,
+  `territory_code` int(11) DEFAULT NULL,
+  `is_steam3_subscription` tinyint(1) DEFAULT NULL,
+  `extended_info` text,
+  PRIMARY KEY (`cdr_id`,`sub_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `sub_state_capture`
+--
+
