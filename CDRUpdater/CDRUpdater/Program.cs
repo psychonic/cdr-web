4ab2b801b6086804495bf3ef97c7d6c684c04f65
Ryan Kistner <azuisleet@gmail.com>
2011-05-08 05:25:10 -0600

    app/sub capture

--- CDRUpdater/CDRUpdater/Program.cs
+++ CDRUpdater/CDRUpdater/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using SteamKit2;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace CDRUpdater
 {
@@ -42,9 +44,22 @@ namespace CDRUpdater
                 }
                 else
                 {
-                    File.WriteAllBytes(CDRBLOB, cdr);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                DebugLog.Write("Unable to download CDR: {0}\n", ex.ToString());
+                return;
+            }*/
+
+            byte[] cdr = File.ReadAllBytes(CDRBLOB);
 
                     byte[] current_hash = CryptoHelper.SHAHash(cdr);
+
+                    string hash_hex = current_hash.Aggregate(new StringBuilder(), 
+                               (sb,v)=>sb.Append(v.ToString("x2"))
+                              ).ToString();
 
                     try
                     {
@@ -54,13 +69,6 @@ namespace CDRUpdater
                     {
                         DebugLog.Write("Warning: Unable to write to hashfile: {0}\n", ex2.ToString());
                     }
-                }
-            }
-            catch (Exception ex)
-            {
-                DebugLog.Write("Unable to download CDR: {0}\n", ex.ToString());
-                return;
-            }*/
 
             DebugLog.Write("Parsing CDR...\n");
 
@@ -79,24 +87,98 @@ namespace CDRUpdater
                 return;
             }
 
-            using (StreamWriter sw = new StreamWriter(new FileStream("output.sql", FileMode.Create)))
+            DebugLog.Write("Building updates..\n");
+
+
+            MySqlConnection connection = new MySqlConnection(connection_string);
+            connection.Open();
+
+            MySqlCommand command;
+
+
+            // insert CDR
+            command = connection.CreateCommand();
+
+            // the current CDR is always the most recently processed, incase we delete CDRs
+            command.CommandText = "SELECT id FROM cdr ORDER BY date_processed DESC LIMIT 1";
+
+            int prev_cdr_id = Convert.ToInt32(command.ExecuteScalar());
+
+            command.CommandText = String.Format("INSERT INTO cdr (hash, version, date_updated, date_processed) VALUES ('{0}', {1}, '{2}', '{3}')",
+                                                    hash_hex, CDRBlob.VersionNum, String.Format("{0:u}", CDRBlob.LastChangedExistingAppOrSubscriptionTime.ToDateTime()),
+                                                    String.Format("{0:u}", DateTime.Now));
+
+            command.ExecuteNonQuery();
+
+            command.CommandText = "SELECT last_insert_id()";
+
+            int cdr_id = Convert.ToInt32(command.ExecuteScalar());
+
+
+            // capture app data and add app_* tables
+            DataSet data = new DataSet();
+            command = connection.CreateCommand();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+
+
+            using (StreamWriter sw_app = new StreamWriter(new FileStream("app.data", FileMode.Create)))
+            using (StreamWriter sw_app_capture = new StreamWriter(new FileStream("app_capture.data", FileMode.Create)))
             {
                 foreach (App app in CDRBlob.Apps)
                 {
-                    //sw.WriteLine(SQLQuery.BuildInsertQueryFromType("app", app));
+                    command.CommandText = "SELECT * FROM app WHERE app_id = " + app.AppID;
+                    data.Clear();
+                    adapter.Fill(data);
+
+                    var app_info = data.Tables[0].Rows[0];
+                    
+                    string app_current_data = null;
+                    string app_state_data = null;
+
+                    SQLQuery.BuildDataInsertFromTypeWithChanges(app, app_info, cdr_id, prev_cdr_id, out app_current_data, out app_state_data);
+
+                    sw_app.WriteLine(app_current_data);
+
+                    if (app_state_data != null)
+                    {
+                        sw_app_capture.WriteLine(app_state_data);
+                    }
 
                     foreach (AppVersion appv in app.Versions)
                     {
                         //sw.WriteLine(SQLQuery.BuildInsertQueryFromType("app_version", appv));
                     }
-                
-                }
 
-                foreach (Sub sub in CDRBlob.Subs)
-                {
-                    sw.WriteLine(SQLQuery.BuildInsertQueryFromType("sub", sub));
                 }
             }
+
+
+            // capture sub data
+            using (StreamWriter sw_sub = new StreamWriter(new FileStream("sub.data", FileMode.Create)))
+            using (StreamWriter sw_sub_capture = new StreamWriter(new FileStream("sub_capture.data", FileMode.Create)))
+            {
+                foreach (Sub sub in CDRBlob.Subs)
+                {
+                    command.CommandText = "SELECT * FROM sub WHERE sub_id = " + sub.SubID;
+                    data.Clear();
+                    adapter.Fill(data);
+
+                    var sub_info = data.Tables[0].Rows[0];
+
+                    string sub_current_data = null;
+                    string sub_state_data = null;
+
+                    SQLQuery.BuildDataInsertFromTypeWithChanges(sub, sub_info, cdr_id, prev_cdr_id, out sub_current_data, out sub_state_data);
+
+                    sw_sub.WriteLine(sub_current_data);
+
+                    if (sub_state_data != null)
+                    {
+                        sw_sub_capture.WriteLine(sub_state_data);
+                    }
+                }
+            }
+
         }
     }
 }
